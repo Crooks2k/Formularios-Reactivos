764b201ff3c424ded5f8332ea4938c81ef3e9f89
Crooks2k <ciberronaldo@hotmail.com>
2023-07-15 22:16:32 -0500

    feat: [app.component.ts] => Se agregaron las instancias del FormBuilder, Inits del formulario y validaciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Formularios-Reactivos';
+  dataForm!: FormGroup
+  constructor(
+    //Se usa private y readonly para que no se manipule la instancia del FormBuilder
+    private readonly fb: FormBuilder
+    ){ }
+  ngOnInit(): void {
+    this.dataForm = this.initForm()
+  }
+
+  onSubmit = ():void => {
+    console.log("Form ->", this.dataForm.value)
+  }
+  initForm = ():FormGroup => {
+    return this.fb.group({
+      //In second position can declare validations or array of validations
+      name: ["", [Validators.required, Validators.minLength(4), Validators.maxLength(60)]],
+      info: ["", [Validators.minLength(10), Validators.maxLength(200)]],
+      datalist: ["", [Validators.required, ]],
+      password: ["", [Validators.required, Validators.minLength(4)]]
+    })
+  }
 }
