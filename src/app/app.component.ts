4a2426d95f173774720735983577412e04dbd079
Crooks2k <ciberronaldo@hotmail.com>
2023-07-28 12:45:24 -0500

    se cambiaoron las validaciones y estilos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,24 +9,37 @@ import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms'
 export class AppComponent implements OnInit{
   title = 'Formularios-Reactivos';
   dataForm!: FormGroup
+  infoCharacters = 0
+  maxLengthInfo = 200
   constructor(
     //Se usa private y readonly para que no se manipule la instancia del FormBuilder
     private readonly fb: FormBuilder
     ){ }
   ngOnInit(): void {
     this.dataForm = this.initForm()
+
+    this.dataForm.get('info')?.valueChanges.subscribe((value) => {
+      this.infoCharacters = value.length;
+    });
   }
 
-  onSubmit = ():void => {
-    console.log("Form ->", this.dataForm.value)
+  onSubmit ():void {
+    alert(`Form Data =>
+    Name: ${this.dataForm.value.name}
+    Email: ${this.dataForm.value.email}
+    Info: ${this.dataForm.value.info}
+    DataList: ${this.dataForm.value.datalist}
+    Password: ${this.dataForm.value.password}`
+    )
   }
   initForm = ():FormGroup => {
     return this.fb.group({
       //In second position can declare validations or array of validations
-      name: ["", [Validators.required, Validators.minLength(4), Validators.maxLength(60)]],
-      info: ["", [Validators.minLength(10), Validators.maxLength(200)]],
-      datalist: ["", [Validators.required, ]],
-      password: ["", [Validators.required, Validators.minLength(4)]]
+      name: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(60)]],
+      email: ['', [Validators.required, Validators.maxLength(30), Validators.minLength(5), Validators.pattern(/^[\w.-]+@[a-zA-Z\d.-]+\.[a-zA-Z]{2,}$/i)]],
+      info: ['', [Validators.minLength(10), Validators.maxLength(this.maxLengthInfo)]],
+      datalist: ['', [Validators.required, ]],
+      password: ['', [Validators.required,Validators.minLength(4)]]
     })
   }
 }
